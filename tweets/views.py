3df3f4727010798eb6a83ab257c239e58b830431
John Parks <jcparks1984@gmail.com>
2022-06-01 13:29:32 -0400

    Formatted Tweet Function + Added Like Button

--- tweets/views.py
+++ tweets/views.py
@@ -1,20 +1,35 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+
 from .models import Tweet
 
+# Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+
 def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
-    data ={
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
+    data = {
         "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
 
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     data = {
         "id": tweet_id,
     }
@@ -25,4 +40,4 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     except:
         data['message'] = "Not found"
         status = 404
-    return JsonResponse(data, status=status)
+    return JsonResponse(data, status=status) 
